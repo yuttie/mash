3a323f51e2346d43b10e37e66614501ba213d6b5
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-09-01 19:37:15 +0900

    Add general (context-independent) commands.

--- pled.hs
+++ pled.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE Rank2Types #-}
 module Main where
 
 import Control.Monad
@@ -12,37 +13,48 @@ import System.IO
 data Editor a = Editor
     { edSource :: Source IO String
     , edPipe :: Conduit String IO a
-    , edCommands :: Map String (Command a)
+    , edCommands :: Map String GCommand
+    , edCtxCommands :: Map String (CCommand a)
     }
 
-data Command a = forall b. Show b => Command (Editor a -> Editor b)
+data GCommand = forall b. Show b => GCommand (forall a. Editor a -> Editor b)
+data CCommand a = forall b. Show b => CCommand (Editor a -> Editor b)
+
+-- General commands
+generalCommands :: Map String GCommand
+generalCommands = Map.fromList [("resetpl", gcmdResetPipeline)]
+
+gcmdResetPipeline :: GCommand
+gcmdResetPipeline = GCommand $ \st -> st { edPipe = edPipe initState
+                                         , edCtxCommands = edCtxCommands initState
+                                         }
 
 -- String commands
-stringCommands :: Map String (Command String)
+stringCommands :: Map String (CCommand String)
 stringCommands = Map.fromList [("append", cmdAppend1), ("toint", cmdToInt)]
 
-cmdAppend1 :: Command String
-cmdAppend1 = Command $ \st -> st { edPipe = edPipe st =$= C.map (++ "1") }
+cmdAppend1 :: CCommand String
+cmdAppend1 = CCommand $ \st -> st { edPipe = edPipe st =$= C.map (++ "1") }
 
-cmdToInt :: Command String
-cmdToInt = Command $ \st -> st { edPipe = edPipe st =$= C.map read
-                               , edCommands = intCommands
+cmdToInt :: CCommand String
+cmdToInt = CCommand $ \st -> st { edPipe = edPipe st =$= C.map read
+                               , edCtxCommands = intCommands
                                }
 
 -- Int commands
-intCommands :: Map String (Command Int)
+intCommands :: Map String (CCommand Int)
 intCommands = Map.fromList [("double", cmdDouble), ("tostr", cmdToStr)]
 
-cmdDouble :: Command Int
-cmdDouble = Command $ \st -> st { edPipe = edPipe st =$= C.map (2 *) }
+cmdDouble :: CCommand Int
+cmdDouble = CCommand $ \st -> st { edPipe = edPipe st =$= C.map (2 *) }
 
-cmdToStr :: Command Int
-cmdToStr = Command $ \st -> st { edPipe = edPipe st =$= C.map show
-                               , edCommands = stringCommands
+cmdToStr :: CCommand Int
+cmdToStr = CCommand $ \st -> st { edPipe = edPipe st =$= C.map show
+                               , edCtxCommands = stringCommands
                                }
 
 process :: Show a => Editor a -> IO ()
-process st@(Editor s p cs) = do
+process st@(Editor s p gcs ccs) = do
     -- output the current state
     input <- s $$ C.consume
     output <- s $= p $$ C.consume
@@ -53,17 +65,20 @@ process st@(Editor s p cs) = do
     hFlush stdout
     -- execute a command
     l <- getLine
-    case Map.lookup l cs of
-        Nothing -> do
-            hPutStrLn stderr $ "Unknown command " ++ show l ++ "."
-            process st
-        Just (Command f) -> process $ f st
+    case Map.lookup l ccs of
+        Nothing -> case Map.lookup l gcs of
+            Nothing -> do
+                hPutStrLn stderr $ "Unknown command " ++ show l ++ "."
+                process st
+            Just (GCommand f) -> process $ f st
+        Just (CCommand f) -> process $ f st
 
 initState :: Editor String
 initState = Editor
     { edSource = C.sourceList $ map show ([1..10]::[Int])
     , edPipe = awaitForever yield
-    , edCommands = stringCommands
+    , edCommands = generalCommands
+    , edCtxCommands = stringCommands
     }
 
 main :: IO ()
