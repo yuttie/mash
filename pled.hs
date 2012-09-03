9b52ce57df392fef5c8ca2740a1bd8f2bcab98f9
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-09-03 21:00:40 +0900

    Refactoring: renaming `State' to `Editor'.

--- pled.hs
+++ pled.hs
@@ -9,36 +9,36 @@ import qualified Data.Map as Map
 import System.IO
 
 
-data State a = State
+data Editor a = Editor
     { source :: Source IO String
     , pipe :: Conduit String IO a
     , commands :: Map String (Command a)
     }
 
-data Command a = forall b. Show b => Command (State a -> State b)
+data Command a = forall b. Show b => Command (Editor a -> Editor b)
 
 -- String commands
 stringCommands :: Map String (Command String)
 stringCommands = Map.fromList [("append", commandAppend1), ("toint", commandToInt)]
 
 commandAppend1 :: Command String
-commandAppend1 = Command $ \(State s p cs) -> State s (p =$= C.map (++ "1")) cs
+commandAppend1 = Command $ \(Editor s p cs) -> Editor s (p =$= C.map (++ "1")) cs
 
 commandToInt :: Command String
-commandToInt = Command $ \(State s p _) -> State s (p =$= C.map read) intCommands
+commandToInt = Command $ \(Editor s p _) -> Editor s (p =$= C.map read) intCommands
 
 -- Int commands
 intCommands :: Map String (Command Int)
 intCommands = Map.fromList [("double", commandDouble), ("tostr", commandToStr)]
 
 commandDouble :: Command Int
-commandDouble = Command $ \(State s p cs) -> State s (p =$= C.map (2 *)) cs
+commandDouble = Command $ \(Editor s p cs) -> Editor s (p =$= C.map (2 *)) cs
 
 commandToStr :: Command Int
-commandToStr = Command $ \(State s p _) -> State s (p =$= C.map show) stringCommands
+commandToStr = Command $ \(Editor s p _) -> Editor s (p =$= C.map show) stringCommands
 
-process :: Show a => State a -> IO ()
-process st@(State s p cs) = do
+process :: Show a => Editor a -> IO ()
+process st@(Editor s p cs) = do
     -- output the current state
     input <- s $$ C.consume
     output <- s $= p $$ C.consume
@@ -55,8 +55,8 @@ process st@(State s p cs) = do
             process st
         Just (Command f) -> process $ f st
 
-initState :: State String
-initState = State
+initState :: Editor String
+initState = Editor
     { source = C.sourceList $ map show ([1..10]::[Int])
     , pipe = awaitForever yield
     , commands = stringCommands
