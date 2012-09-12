d0a25300dece2fd6f58819deb1751151e25c0e3f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-09-12 17:55:11 +0900

    Add `runServer' and `runClient' for launching a server and a client for IPC.

--- pled.hs
+++ pled.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM
-import Control.Monad (forever)
+import Control.Exception.Lifted (bracket, finally)
+import Control.Monad (forever, void, when)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Control (MonadBaseControl, control)
 import Data.Conduit
 import qualified Data.Conduit.List as C
+import qualified Data.Conduit.Network as CN
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified Network.Socket as NS
+import System.Directory (doesFileExist, removeFile)
 import System.IO
 
 import Manipulator
@@ -29,6 +36,47 @@ initState = Manipulator
     , manipCommands = generalCommands
     , manipCtxCommands = stringCommands
     }
+
+deleteFile :: FilePath -> IO ()
+deleteFile fp = do
+    exist <- doesFileExist fp
+    when exist $
+        removeFile fp
+
+runServer :: (MonadIO m, MonadBaseControl IO m) => FilePath -> CN.Application m -> m ()
+runServer fp app = bracket
+    (liftIO prepareSocket)
+    (liftIO . NS.sClose)
+    (forever . serve)
+  where
+    prepareSocket = do
+        lsock <- NS.socket NS.AF_UNIX NS.Stream NS.defaultProtocol
+        deleteFile fp
+        NS.bindSocket lsock $ NS.SockAddrUnix fp
+        NS.listen lsock NS.maxListenQueue
+        return lsock
+    serve lsock = do
+        (sock, _) <- liftIO $ NS.accept lsock
+        let src = CN.sourceSocket sock
+        let sink = CN.sinkSocket sock
+        control $ \run -> do
+            _ <- forkIO $ void $ run (app src sink) `finally` NS.sClose sock
+            run $ return ()
+
+runClient :: (MonadIO m, MonadBaseControl IO m) => FilePath -> CN.Application m -> m ()
+runClient fp app = bracket
+    (liftIO prepareSocket)
+    (liftIO . NS.sClose)
+    (doSomething)
+  where
+    prepareSocket = do
+        sock <- NS.socket NS.AF_UNIX NS.Stream NS.defaultProtocol
+        NS.connect sock $ NS.SockAddrUnix fp
+        return sock
+    doSomething sock = do
+        let src = CN.sourceSocket sock
+        let sink = CN.sinkSocket sock
+        app src sink
 
 main :: IO ()
 main = do
