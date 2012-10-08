ef279cf3482a171b05670fc12f35c692edde5466
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2012-10-08 17:56:57 +0900

    A Bytes stream now can be decoded into a Text stream.

--- Manipulator/Command/Bytes.hs
+++ Manipulator/Command/Bytes.hs
@@ -1,14 +1,20 @@
 module Manipulator.Command.Bytes
     ( -- Bytes commands
       cmdAppendBytes
+    , cmdDecodeUtf8
     ) where
 
 import qualified Data.ByteString as B
 import Data.Conduit (GInfConduit, (=$=), awaitForever, yield)
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Text as CT
+import Data.Map (Map)
 import Data.Maybe (listToMaybe)
+import Data.Text (Text)
 import Data.Word (Word8)
 
 import Manipulator.Core
+import Manipulator.Stream.Text ()
 
 
 append :: Monad m => a -> GInfConduit a m a
@@ -27,3 +33,10 @@ cmdAppendBytes = Command $ \st args ->
     case mapM maybeRead args :: Maybe [Word8] of
         Just bs -> Right $ st { manipPipe = manipPipe st =$= append (Bytes $ B.pack bs) }
         Nothing -> Left $ CommandArgumentError args
+
+cmdDecodeUtf8 :: Map String (Command Text) -> Command Bytes
+cmdDecodeUtf8 textCmds = Command $ \st args -> case args of
+    [] -> Right $ st { manipPipe = manipPipe st =$= CL.map unbytes =$= CT.decode CT.utf8
+                     , manipCtxCommands = textCmds
+                     }
+    _ -> Left $ CommandArgumentError args
